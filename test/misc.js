3ad3c0ed0df3c425f43383b9ae60f2e8140ac207
Goran Topic <telegonicaxx@live.com>
2023-07-05 11:56:44 -0700

    added misc test

---
+++ test/misc.js
@@ -0,0 +1,15 @@
+import Checklist from '../index.js'
+import assert from 'assert';
+import chai from 'chai';
+const expect = chai.expect
+
+describe('return 0 when there are no done values', () => {
+    let values = [ '🥚 eggs', '🥩 ham', '🧀 cheese', '🍎 apple', '🥦 broccoli' ];
+    let checklist = new Checklist(values, { 
+        name: 'return 0 when done is empty',
+    });
+    // values done
+    it('valuesDone() === 0', () => 
+        assert.equal(checklist.valuesDone(), 0)
+    )
+});
